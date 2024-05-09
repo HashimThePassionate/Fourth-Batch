babdbfda98fe54598fa63e7d37881de1c5615118
Muhammad Hashim <hashiimtahir@gmail.com>
2024-05-09 10:59:02 +0500

    JS Part1

---
+++ Day13/script.js
@@ -0,0 +1,24 @@
+// String -- ' ' , " ", ` `
+// Number 1,1.2,-1.2,-3
+// Boolean True,1, false,0
+
+//  let, var for variable declaration
+
+// var my_name = prompt('Please Enter Your Name') // User input 
+// myName, MyName, _myname, my_name
+var my_name  = 'Hashim'
+console.log(`My name is ${my_name} ${typeof(my_name)}`);
+var pi = 3.149999
+// var radius = parseFloat(prompt('please Enter value of radius'))
+var radius = 4.9
+message = `
+Type of radius ${typeof(radius)}
+Value of pi = ${pi}
+Value of radius = ${radius}
+Area of circle ${pi} * ${radius} * ${radius} = ${pi * radius * radius}
+`
+
+console.log(message);
+// console.log(typeof(message));
+
+
