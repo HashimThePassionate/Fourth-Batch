81def3f364eb2a97c6c365d5e91c30650307dda1
Muhammad Hashim <hashiimtahir@gmail.com>
2024-05-10 11:23:38 +0500

    JS Operators and Decisison Statements

---
+++ Day14/script.js
@@ -0,0 +1,82 @@
+// Assignment Operator
+//  =
+var Name = "Hashim"
+// Arithemetic Operator
+// *,+,-,/
+// Arithmetic Assignment
+// +=,-=,*=,/=
+
+// let a = 10
+// let b= 5
+// console.log(a);
+// a = a + a
+// a += b
+// console.log(a);
+
+// Comparison Operators 
+// True '1' / false '0'
+// >,>=,<,<=,==,===
+// z = 10
+// console.log(a >= z);
+
+// Logical Operator
+// && , ! , |
+
+// console.log((1 > 2) || (12< 13));
+// console.log(!(1 > 2));
+
+// let age = 5
+// if(age >= 18){
+// console.log('Hello You are in if statement');    
+// }
+// else
+// console.log('You are in else block');
+
+
+// let per = 30
+// if((per>=90)&& (per <= 100))
+//     console.log('A+ Grade');
+// else if((per>=80)&& (per < 90))
+//     console.log('A Grade');
+// else if((per>=70)&& (per < 80))
+//     console.log('B Grade');
+// else if((per>=60)&& (per < 70))
+//     console.log('D Grade');
+// else if((per>=50)&& (per < 60))
+//     console.log('Pass');
+// else if((per>=0)&& (per < 50))
+//     console.log('Fail');
+// else 
+// console.log('Invalid');
+
+// let n1 = 10, n2 = 2, n3 = 4
+// if (n1 > n2) {
+//     if (n1 > n3) {
+//         console.log('n1 is greater than n2 and n3');
+//     } else {
+//         console.log('n1 is greater than n2 but not than n3');
+//     }
+    
+// } else {
+//     console.log('n1 is not greater than n2 and n3');
+    
+// }
+
+// baby = 10
+// g = 'girl'
+// doll = 'mackup'
+// if(baby == 10)
+//     if(g == 'boy')
+//         console.log('Blue Cycle');
+//     else if (g = 'girl')
+//         {
+//             if(doll =='dool')
+//                 console.log('pink cycle');
+//             else 
+//             console.log('No cycle but doll');
+//         }
+// else {
+//     console.log('No gift');
+// }
+
+// Switch Case
