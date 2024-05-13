3f7def4fe92fd8de7e2ce25f63267105a23e7c8f
Muhammad Hashim <hashiimtahir@gmail.com>
2024-05-13 10:41:15 +0500

    Functions

---
+++ Day15/script.js
@@ -0,0 +1,24 @@
+// function display()
+// {
+//     return 'Hello World'
+// }
+// console.log(display());
+// a = display()
+// console.log(a);
+
+function Addition(x, y) {
+    return x + y
+}
+
+console.log(Addition(20, 10));
+console.log(Addition('Hello', 'World'));
+
+function area_of_circle(r) {
+    let pi = 3.14
+    return pi * r * r
+}
+
+console.log(`Area of Circle is ${area_of_circle(4)}`);
+console.log(`Area of Circle is ${area_of_circle(2)}`);
+console.log(`Area of Circle is ${area_of_circle(10)}`);
+console.log("Area of circle is ", area_of_circle(5));
