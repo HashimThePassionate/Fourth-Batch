bc5400091704417688d692156d9017fc74323e8b
Muhammad Hashim <hashiimtahir@gmail.com>
2024-05-14 10:31:35 +0500

    Anonymous Function

---
+++ Day16/script.js
@@ -0,0 +1,22 @@
+// display()
+// function display() {
+//     console.log('Hello world');
+// }
+// // Anpnymous function
+
+// let a = function(name)
+// {
+//     return `Hey ${name}`
+// }
+// console.log(a('Hashim'));
+
+// (function()
+// {
+//     console.log('Hello world');
+// })();
+
+// Arrow function 
+let AOC = (r,pi) => pi*r*r
+
+console.log(`Area of Circle is ${AOC(4,3.14)}`);
+console.log(`Area of Circle is ${AOC(2,3.14)}`);
