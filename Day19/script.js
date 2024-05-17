a67f50413010cc56a00104a4745873436a9d532d
Muhammad Hashim <hashiimtahir@gmail.com>
2024-05-17 11:01:50 +0500

    Loops

---
+++ Day19/script.js
@@ -0,0 +1,65 @@
+// let table = 5
+// for(let i = 1; i<=20; i++)
+// {
+//     console.log();
+//     console.log(` ${table} * ${i} = ${table*i} `);
+// }
+
+// grocery = ['milk','eggs','rice','Meat','wheat']
+// console.log(grocery);
+// for(i = 0; i<=grocery.length;i++)
+//     {
+//         if(grocery[i] == 'rice')
+//             {
+//                 console.log(grocery[i].toUpperCase());
+//             }
+//         else
+//             console.log(grocery[i]);
+//     }
+
+// for(k in person)
+//     {
+//         if(person[k] == 'Muhammad')
+//             console.log(person[k].toUpperCase());
+//         else if(person[k] == 63)
+//             console.log(`${person[k]+10}`);
+//         else if(person[k] === 'function')
+//             {
+//                 a = person[k];
+//                 a()
+//             }
+
+//         else
+//             console.log(person[k]);
+
+
+//     }
+
+// let person = {
+//     firstName: 'Muhammad',
+//     lastname:'Hashim',
+//     age: 63,
+//     occupation: 'Teacher',
+// };
+
+// for (let k in person) {
+//     if (person[k].toString().toLowerCase() == person.occupation.toLowerCase()) {
+//         console.log(person[k].toUpperCase());
+//     }
+//     else {
+//         console.log(person[k]);
+//     }
+// }
+
+// let name = 'Hashim'
+// console.log(name[0]);
+
+
+// let marks = [50,25,63,78,90]
+// for(m of marks)
+//     {
+//         if(m == 63)
+//             console.log(`urdu Subject Marks : ${m+10}`);
+//         else
+//             console.log(`Marks : ${m}`);
+//     }
